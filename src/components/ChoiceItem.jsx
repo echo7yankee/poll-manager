6b242b2c826e929a14300a739338988f8c5743a2
Dragos Ifrim <dragosifrim@Dragoss-MacBook-Pro.local>
2019-01-07 15:18:40 +0200

    Fix #1: renamed React components from .js to .jsx

--- src/components/ChoiceItem.jsx
+++ src/components/ChoiceItem.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import "./polls.css";
 
-const ChoiceItem = props => {
+const ChoiceItem = ({
+  deleteNewChoice,
+  handleChoiceInput,
+  choiceInput,
+  index,
+  ChoicesLength
+}) => {
   return (
     <div className="polls_choices">
-      <label className="polls_label">Choice #</label>
-      <input className="polls_input-choice" type="text" />
-      <span className="btn-trash" onClick={props.deleteNewChoice}>
-        <i className="fas fa-trash" />
-      </span>
+      <label className="polls_label">Choice # {index}</label>
+      <input
+        className="polls_input-choice"
+        type="text"
+        value={choiceInput}
+        onChange={handleChoiceInput}
+      />
+      {index > 2 && (
+        <span className="btn-trash" onClick={deleteNewChoice}>
+          <i className="fas fa-trash" />
+        </span>
+      )}
     </div>
   );
 };
--- src/components/Choices.jsx
+++ src/components/Choices.jsx
@@ -5,9 +5,15 @@ import uuid from "uuid";
 class Choices extends Component {
   state = {
     newChoices: [],
-    id: uuid(),
-    choiceInput: ""
+    choiceInput: {},
+    id: uuid()
   };
+
+  componentWillMount() {
+    this.setState({
+      newChoices: [0, 1]
+    });
+  }
 
   handleChoiceInput = e => {
     this.setState({
@@ -15,7 +21,7 @@ class Choices extends Component {
     });
   };
 
-  addNewChoice = e => {
+  addNewChoice = () => {
     const newChoice = {
       id: this.state.id,
       choiceInput: this.state.choiceInput
@@ -24,36 +30,41 @@ class Choices extends Component {
     const updatedChoices = [...this.state.newChoices, newChoice];
     this.setState({
       newChoices: updatedChoices,
-      choiceInput: "",
       id: uuid()
     });
   };
 
   clearNewChoices = () => {
     this.setState({
-      newChoices: []
+      newChoices: [0, 1]
     });
   };
 
   deleteNewChoice = id => {
-    const filteredChoices = this.state.newChoices.filter(newChoice => {
-      return newChoice.id !== id;
-    });
+    if (this.state.newChoices.length > 2) {
+      const filteredChoices = this.state.newChoices.filter(newChoice => {
+        return newChoice.id !== id;
+      });
 
-    this.setState({
-      newChoices: filteredChoices
-    });
+      this.setState({
+        newChoices: filteredChoices
+      });
+    }
   };
 
   render() {
     return (
       <div className="polls_choices-container">
-        {this.state.newChoices.map(newChoice => {
+        {this.state.newChoices.map((newChoice, index) => {
+          console.log(newChoice.id);
           return (
             <ChoiceItem
               key={newChoice.id}
-              choiceInput={newChoice.choiceInput}
+              choiceInput={this.state.choiceInput[newChoice.id]}
+              handleChoiceInput={this.handleChoiceInput}
               deleteNewChoice={() => this.deleteNewChoice(newChoice.id)}
+              index={index + 1}
+              ChoicesLength={this.state.newChoices.length}
             />
           );
         })}
@@ -61,7 +72,7 @@ class Choices extends Component {
         <button className="add_choice" onClick={this.addNewChoice}>
           + Add new choice
         </button>
-        {this.state.newChoices.length > 0 && (
+        {this.state.newChoices.length > 2 && (
           <button className="add_choice" onClick={this.clearNewChoices}>
             Clear new choices
           </button>
