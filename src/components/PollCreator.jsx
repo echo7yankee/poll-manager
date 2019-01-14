a0c02a896aeeb6d10a75bffa53b863080aea6b65
Dragos <cosminifrim55@yahoo.com>
2019-01-14 11:37:05 +0200

    Read Description
    Added logic for rendering the multiple choice or not.
    Added logic to pass the question value into the PollResults component.

--- src/components/PollCreator.jsx
+++ src/components/PollCreator.jsx
@@ -4,8 +4,43 @@ import "./polls.css";
 import Choices from "./CreateNewChoices/Choices.jsx";
 
 class PollCreator extends Component {
+  constructor(props) {
+    super(props);
+    this.question = React.createRef();
+    this.state = {
+      value: "",
+      renderError: false,
+      visible: false,
+      invisible: true
+    };
+  }
   handleSubmit = e => {
     e.preventDefault();
+    if (this.question.current.value === "") {
+      this.setState({
+        renderError: true
+      });
+    } else {
+      this.setState({
+        value: this.question.current.value,
+        renderError: false
+      });
+    }
+
+    this.question.current.value = "";
+  };
+  handleRadioChange = e => {
+    this.setState({
+      invisible: !this.state.invisible,
+      visible: !this.state.visible
+    });
+  };
+  renderChoices = () => {
+    if (this.state.visible) {
+      return <Choices />;
+    } else if (this.state.invisible) {
+      return null;
+    }
   };
 
   render() {
@@ -29,6 +64,40 @@ class PollCreator extends Component {
               <div className="radio__label-container">
                 <input className="polls-radio" type="radio" />
                 <label>Multiple choice form</label>
+          <form className="polls-form" onSubmit={this.handleSubmit}>
+            <div className="polls__inputs-container">
+              <label className="polls-label">Question:</label>
+              <input className="polls-input" type="text" ref={this.question} />
+            </div>
+            {this.state.renderError && <p>Please insert a value</p>}
+            <div className="polls__inputs-container">
+              <label className="polls-label">Answers:</label>
+              <div className="polls__radio-container">
+                <div className="radio__label-container">
+                  <label className="mt-2">
+                    <input
+                      className="polls-radio"
+                      type="radio"
+                      value="invisibleForm"
+                      checked={this.state.invisible}
+                      onChange={this.handleRadioChange}
+                    />
+                    Yes/No Form
+                  </label>
+                </div>
+
+                <div className="radio__label-container">
+                  <label className="mt-2">
+                    <input
+                      className="polls-radio"
+                      type="radio"
+                      value="visibleForm"
+                      checked={this.state.visible}
+                      onChange={this.handleRadioChange}
+                    />
+                    Multiple choice form
+                  </label>
+                </div>
               </div>
             </div>
           </div>
@@ -40,6 +109,8 @@ class PollCreator extends Component {
           </div>
         </form>
       </div>
+            {this.renderChoices()}
+        <PollResults value={this.state.value} />
     );
   }
 }
