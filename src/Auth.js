c86902cea8808165311d399d90f6a1ea697b33c2
Dragos <cosminifrim55@yahoo.com>
2019-02-14 13:47:21 +0200

    Deleted comment

--- src/Auth.js
+++ src/Auth.js
@@ -19,15 +19,3 @@ class Auth {
 }
 
 export default new Auth();
-
-// export default {
-//   isAuthenticated: false,
-//   authenticate(cb) {
-//     this.isAuthenticated = true;
-//     setTimeout(cb, 100); // fake async
-//   },
-//   signout(cb) {
-//     this.isAuthenticated = false;
-//     setTimeout(cb, 100);
-//   }
-// };
