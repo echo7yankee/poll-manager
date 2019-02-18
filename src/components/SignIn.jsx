b5ac7ba5f9f95cd84fb9944f2ff1354298a3bfff
Dragos <cosminifrim55@yahoo.com>
2019-02-18 12:10:48 +0200

    Added style to auth buttons

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./navbar.css";
+import { GooglePlusIcon } from "./reusableComponents/icons";
 
 const SignIn = props => {
   return (
     <nav className="nav">
-      <button
-        className="ui red google button google-button"
-        onClick={props.onSignInClick}
-      >
-        <i className="google icon" />
+      <button className="google-button" onClick={props.onSignInClick}>
+        <GooglePlusIcon />
         Sign In with Google
       </button>
     </nav>
--- src/components/SignOut.jsx
+++ src/components/SignOut.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./navbar.css";
 import PollCreatorLink from "./PollCreatorLink";
+import { GooglePlusIcon } from "./reusableComponents/icons";
 
 const SignOut = props => {
   return (
     <nav className="nav">
-      <button
-        onClick={props.onSignOutClick}
-        className="ui red google button google-button"
-      >
-        <i className="google icon" />
+      <button onClick={props.onSignOutClick} className="google-button">
+        <GooglePlusIcon />
         Sign Out
       </button>
       <li className="nav-item">
