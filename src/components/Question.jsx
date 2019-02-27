6504ba4f129e68aba6c634ef5ae0487dd30e5432
Dragos <cosminifrim55@yahoo.com>
2019-02-27 14:59:09 +0200

    Refactore component to stateless component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import PollQuestionChoices from "./PollQuestions/PollQuestionChoices";
 
@@ -8,30 +8,23 @@ import "./polls.css";
 import "./PollQuestions/pollsQuestion.css";
 import RadioInput from "./reusableComponents/RadioInput";
 
-class Question extends Component {
-  state = {
-    question: this.props.question
-  };
-
-  renderAnswers = () => {
-    const { type, answers } = this.state.question;
-    const { question } = this.state;
-
-    if (type === YES_NO) {
+const Question = ({ question, setRadio, setCheckbox, selected }) => {
+  const renderAnswers = () => {
+    if (question.type === YES_NO) {
       return (
         <div className="polls__answers-yesNo">
           <RadioInput
             name={question.id}
             value={YES}
-            type={this.props.selected === YES}
-            onChange={e => this.props.setRadio(e.target.value, question.id)}
+            type={selected === YES}
+            onChange={e => setRadio(e.target.value, question.id)}
             text={"Yes"}
           />
           <RadioInput
             name={question.id}
             value={NO}
-            type={this.props.selected === NO}
-            onChange={e => this.props.setRadio(e.target.value, question.id)}
+            type={selected === NO}
+            onChange={e => setRadio(e.target.value, question.id)}
             text={"No"}
           />
         </div>
@@ -39,31 +32,27 @@ class Question extends Component {
     } else {
       return (
         <PollQuestionChoices
-          type={type}
-          answers={answers}
-          id={this.state.question.id}
-          setRadio={this.props.setRadio}
-          setCheckbox={this.props.setCheckbox}
-          selected={this.props.selected}
+          type={question.type}
+          answers={question.answers}
+          id={question.id}
+          setRadio={setRadio}
+          setCheckbox={setCheckbox}
+          selected={selected}
         />
       );
     }
   };
 
-  render() {
-    const { value } = this.state.question;
+  return (
+    <div className="polls__question">
+      <label className="polls-label">Question:</label>
+      <p className="polls-text">{question.value}</p>
 
-    return (
-      <div className="polls__question">
-        <label className="polls-label">Question:</label>
-        <p className="polls-text">{value}</p>
-
-        <div className="polls__questions polls__question--colstart2">
-          {this.renderAnswers()}
-        </div>
+      <div className="polls__questions polls__question--colstart2">
+        {renderAnswers()}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Question;
