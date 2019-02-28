843baf76455899ce69196fa70479b0ee295eb3f0
Dragos <cosminifrim55@yahoo.com>
2019-02-28 14:23:31 +0200

    Refactor: converted component to stateless component

--- src/components/CreateNewChoices/Choices.jsx
+++ src/components/CreateNewChoices/Choices.jsx
@@ -1,49 +1,38 @@
-import React, { Component } from "react";
 import ChoiceItem from "./ChoiceItem.jsx";
 import "./choices.css";
 
-class Choices extends Component {
-  render() {
-    const { answers } = this.props;
+const Choices = ({
+  answers,
+  handleChoiceInput,
+  deleteChoice,
+  addChoice,
+  clearAllChoices
+}) => {
+  return (
+    <>
+      <div className="polls__answer">
+        {answers.map((choice, index) => {
+          return (
+            <ChoiceItem
+              key={choice.id}
+              inputValue={choice.value}
+              handleChoiceInput={e =>
+                handleChoiceInput(e.target.value, choice.id)
+              }
+              deleteChoice={() => deleteChoice(choice.id, index)}
+              index={index + 1}
+            />
+          );
+        })}
+      </div>
 
-    return (
-      <>
-        <div className="polls__answer">
-          {answers.map((choice, index) => {
-            return (
-              <ChoiceItem
-                key={choice.id}
-                inputValue={choice.value}
-                handleChoiceInput={e =>
-                  this.props.handleChoiceInput(e.target.value, choice.id)
-                }
-                deleteChoice={() => this.props.deleteChoice(choice.id, index)}
-                index={index + 1}
-              />
-            );
-          })}
-        </div>
-
-        <div className="polls__answers-buttons">
-          <button
-            className="button-answer"
-            onClick={this.props.addChoice}
-            type="button"
-          >
-            + Add new choice
+      <div className="polls__answers-buttons">
+        <button className="button-answer" onClick={addChoice} type="button">
+          + Add new choice
+        </button>
+        {answers.length > 2 && (
+          <button className="button-answer" onClick={clearAllChoices}>
+            Clear Choices
           </button>
-          {answers.length > 2 && (
-            <button
-              className="button-answer"
-              onClick={this.props.clearAllChoices}
-            >
-              Clear Choices
-            </button>
-          )}
-        </div>
-      </>
-    );
-  }
-}
 
 export default Choices;
